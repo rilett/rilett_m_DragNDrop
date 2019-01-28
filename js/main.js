7f57609db4622c47bb5f57d4d277f025e1e7b72f
Rilett <m_rilett@tss-hr420-dm36.fc.ca>
2019-01-28 15:12:58 -0500

    js start

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(() => {
+	window.addEventListener("load", changeHeaderText);
+
+	function changeHeaderText() {
+		document.querySelector('h1').textContent = "hey there from java script"
+	}
+	// set up the puzzle pieces and boards
+	
+})();
